66e6a9306abac4fb358207a2e591de684cad650b
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-09-25 15:06:52 +0300

    Секция с слайдером: слайдер на мобилках без адаптива и с точками

--- js/slider.js
+++ js/slider.js
@@ -7,9 +7,13 @@ const slider = document.querySelector('.slider');
 const sliderLine = document.querySelector('.slider-line'); //Контейнер с картинками слайдера
 const sliderBtnLeft = document.querySelector('.slider-btn--left');
 const sliderBtnRight = document.querySelector('.slider-btn--right');
+const sliderDots = document.querySelectorAll('.slider-dots img');
+const dotInactiveSrc = 'assets/img/icons/dot-inactive.svg';
+const dotActiveSrc = 'assets/img/icons/dot-active.svg';
 
 sliderBtnLeft.addEventListener('click', function() {
 	const photoWidth = slider.offsetWidth;
+
 	setImagesWidth(photoWidth);
 
 	offset -= photoWidth;
@@ -22,6 +26,7 @@ sliderBtnLeft.addEventListener('click', function() {
 });
 sliderBtnRight.addEventListener('click', function() {
 	const photoWidth = slider.offsetWidth;
+
 	setImagesWidth(photoWidth);
 
 	offset += photoWidth;
@@ -29,6 +34,16 @@ sliderBtnRight.addEventListener('click', function() {
 	if (offset > photoWidth*lastImageIndex) {
 		offset = 0;
 	}
+
+	// расчет неверен, тк выдает инвертированную позицию, пофиксил css-ом
+	const position = Math.floor((sliderLine.offsetWidth - offset)/(totalImages * 100) ) - 1;
+	sliderDots.forEach((dot, index) => {
+		if (index === position) {
+			dot.src = 'assets/img/icons/dot-active.svg';
+		} else {
+			dot.src = 'assets/img/icons/dot-inactive.svg';
+		}
+	});
 
 	sliderLine.style.left = -offset + 'px';
 });
